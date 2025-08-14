db9662a71e492c9eafe50107865e87d9a75722ef
urprakashgupta <urprakashgupta@gmail.com>
2025-08-15 00:04:43 +0530

    Implement login form with email and password fields

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,3 +1,30 @@
 export default function Login() {
-  return <div>hello from loign page</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div className="card card-border bg-base-100 w-96">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter Your Email"
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="Enter Your Password"
+            />
+          </fieldset>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
