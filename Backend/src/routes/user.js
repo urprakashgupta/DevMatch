263057f909ebd4470923b8f9ff2f5df4dcd5d8f3
urprakashgupta <urprakashgupta@gmail.com>
2025-12-09 23:02:58 +0530

    feat: add feed endpoint to retrieve users excluding hidden connections

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userAuth from '../middlewares/auth.js';
 import connectionRequest from '../models/connectionRequest.models.js';
+import User from '../models/user.models.js';
 
 
 const router = express.Router();
@@ -53,6 +54,36 @@ router.get('/connections', userAuth, async (req, res) => {
         res.json({ data });
     } catch (error) {
         res.status(400).send("ERROR:" + error.message)
+    }
+});
+
+router.get('/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await connectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select('fromUserId toUserId');
+
+        const hideUserFromFeed = new Set();
+
+        connectionRequests.forEach((req) => {
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUserFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(USER_SAFE_DATA)
+        res.send(users)
+    } catch (error) {
+        res.status(400).json({ message: 'ERROR: ' + error.message });
     }
 })
 
