a9e63b9703b18e531b94e9aeaacc07a6174e3f51
urprakashgupta <urprakashgupta@gmail.com>
2025-12-12 23:59:07 +0530

    feat: remove ThemeToggle component and its usage in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { Footer } from "./components/Footer";
 import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
     <div>
-      <ThemeToggle />
       <Navbar />
       <Footer />
     </div>
